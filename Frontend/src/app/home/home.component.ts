38ebf054822df89d1290bac76700797f51dbb277
RAHULK2308 <rahulkm4002@gmail.com>
2021-07-23 13:43:45 +0530

    Homepage

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -6,6 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+//this is a variable that hold number
+accuratecount:number = 400;
+clientcount:number = 600;
+customerfeedback:number = 0;
+
+accuratecountstop:any = setInterval(()=>{
+  this.accuratecount++;
+  if(this.accuratecount == 849)
+  {
+    
+    clearInterval(this.accuratecountstop);
+  }
+},3) 
+
+
+clientcountstop:any = setInterval(()=>{
+  this.clientcount++;
+  if(this.clientcount == 999)
+  {
+    
+    clearInterval(this.clientcountstop);
+  }
+},0.1)
+
+customerfeedbackstop:any = setInterval(()=>{
+  this.customerfeedback++;
+  if(this.customerfeedback == 100)
+  {
+    
+    clearInterval(this.customerfeedbackstop);
+  }
+},15)
 
   constructor() { }
 
